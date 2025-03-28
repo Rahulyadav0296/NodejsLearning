6c3dfe22db2e06b2e9a830cd00f109659d9bcc25
rahulyadav02996 <77198600+Rahulyadav0296@users.noreply.github.com>
2025-03-28 10:00:08 +0530

    First Commit

--- index.js
+++ index.js
@@ -1,32 +1,12 @@
 require("dotenv").config();
 const express = require("express");
-const cors = require("cors");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const app = express();
+
 app.use(cors());
 
-mongoose
-  .connect(process.env.MONGO_URL)
-  .then(() => {
-    console.log("Mongo Database connected");
-  })
-  .catch((error) => {
-    console.error(error);
-  });
-
-app.get("/", (req, res) => {
-  console.log("Hello Rahul");
-});
-
-app.get("/twitter", (req, res) => {
-  res.send("rahulyadav0296");
-});
-
-app.get("/login", (req, res) => {
-  res.send("<h1>Please login at the webpage</h1>");
-});
-
 app.listen(process.env.PORT || 5000, () => {
-  console.log(`Server running on the PORT of ${process.env.PORT}`);
+  console.log("Server Connected on the port", process.env.PORT);
 });
