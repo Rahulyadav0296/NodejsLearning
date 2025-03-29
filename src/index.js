7ff91b054d121ac2c83eb93762c7eb66e53f1083
rahulyadav02996 <77198600+Rahulyadav0296@users.noreply.github.com>
2025-03-29 17:57:19 +0530

    database connection

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 require("dotenv").config();
 const express = require("express");
-const mongoose = require("mongoose");
+const connectDB = require("./db/index.js");
 const cors = require("cors");
 
 const app = express();
-
 app.use(cors());
+connectDB();
+// (async () => {
+//   try {
+//     await mongoose.connect(process.env.MONGO_URL);
+//     app.on("error", (error) => {
+//       console.log("ERROR", error);
+//       throw error;
+//     });
 
-app.listen(process.env.PORT || 5000, () => {
-  console.log("Server Connected on the port", process.env.PORT);
-});
+//     app.listen(process.env.PORT || 5000, () => {
+//       console.log("Server Connected on the port", process.env.PORT);
+//     });
+//   } catch (error) {
+//     console.error(error);
+//     throw error;
+//   }
+// })();
